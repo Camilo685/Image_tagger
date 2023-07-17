eeb148f5eafe418a50fccb974d4897c254cf4f24
Camilo685 <camiloandres685@gmail.com>
2023-07-16 23:41:55 -0500

    Delete init.py

--- init.py
+++
@@ -1,9 +0,0 @@
-import sys
-from main import main
-from Image_organizer import main as image_organizer_main
-
-if __name__ == "__main__":
-	tmp = sys.executable
-	if main():
-		os.execl(tmp, tmp, * sys.argv)
-	image_organizer_main()
