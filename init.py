535776815608a5992970b52682fbcc861534b941
Camilo685 <camiloandres685@gmail.com>
2023-07-21 15:44:01 -0500

    Update init.py

--- init.py
+++ init.py
@@ -1,8 +1,12 @@
-import sys
+import sys, os
 from main import main
 from Image_organizer import main as image_organizer_main
 
 if __name__ == "__main__":
-	if main():
-		os.execl(sys.executable, os.path.abspath(__file__), *sys.argv)
+	if not len(sys.argv) > 1:
+		update_b, exe_n = main()
+		if update_b:
+			os.execv(sys.executable, [sys.executable, os.getcwd() + "/" + exe_n] + ["(old)" + exe_n])
+	else:
+		os.remove(os.getcwd() + "/" + sys.argv[-1])
 	image_organizer_main()
